openshift/installer-aro

CARRY: allow end user to specify Azure resource group on cluster creation
This allows the RP to specify the cluster's resource group.  TODO: reduce the
scope of this patch to get this upstream; don't allow end-users to choose their
cluster resource group.

pkg/asset/manifests/azure/cloudproviderconfig_test.go
@@ -13,6 +13,7 @@ func TestCloudProviderConfig(t *testing.T) {
 		ResourcePrefix:           "clusterid",
 		SubscriptionID:           "subID",
 		TenantID:                 "tenantID",
+		ResourceGroup:            "clusterid-rg",
 		NetworkResourceGroupName: "clusterid-rg",
 		NetworkSecurityGroupName: "clusterid-node-nsg",
 		VirtualNetworkName:       "clusterid-vnet",

jim-minter/installer

pkg/asset/manifests/azure/cloudproviderconfig_test.go
@@ -13,6 +13,7 @@ func TestCloudProviderConfig(t *testing.T) {
 		ResourcePrefix:           "clusterid",
 		SubscriptionID:           "subID",
 		TenantID:                 "tenantID",
+		ResourceGroupName:        "clusterid-rg",
 		NetworkResourceGroupName: "clusterid-rg",
 		NetworkSecurityGroupName: "clusterid-node-nsg",
 		VirtualNetworkName:       "clusterid-vnet",